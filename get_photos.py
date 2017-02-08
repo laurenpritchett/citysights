b1f4616a998f18d196636766d7abeaca63d3f5ac
Lauren Pritchett <laurenpritchett2@gmail.com>
2017-02-08 01:43:11 +0000

    search-results successfully shows 10 photos with source links

--- get_photos.py
+++ get_photos.py
@@ -10,27 +10,27 @@ flickr = flickrapi.FlickrAPI(api_key, api_secret)
 
 def get_photos_by_location(lat, lon):
     """Get URL for interesting photos with specified latitude and longitude."""
-    photo_urls = []
+    url_pairs = []
 
     photos = flickr.photos.search(per_page='10', format='json', lat=lat, lon=lon,
-                                  radius='1', sort='interestingness-desc')
+                                  sort='interestingness-desc')
 
     photo_info = json.loads(photos)
 
     for info in range(len(photo_info['photos']['photo'])):
-        photo_id = photo_info['photos']['photo'][0]['id']
-        user_id = photo_info['photos']['photo'][0]['owner']
+        photo_id = photo_info['photos']['photo'][info]['id']
+        user_id = photo_info['photos']['photo'][info]['owner']
         photo_source_template = "https://www.flickr.com/photos/{}/{}/"
         photo_source_url = photo_source_template.format(user_id, photo_id)
 
-        farm_id = photo_info['photos']['photo'][0]['farm']
-        server_id = photo_info['photos']['photo'][0]['server']
-        photo_secret = photo_info['photos']['photo'][0]['secret']
+        farm_id = photo_info['photos']['photo'][info]['farm']
+        server_id = photo_info['photos']['photo'][info]['server']
+        photo_secret = photo_info['photos']['photo'][info]['secret']
 
         photo_url_template = "https://farm{}.staticflickr.com/{}/{}_{}.jpg"
         photo_url = photo_url_template.format(farm_id, server_id, photo_id, photo_secret)
-        photo_urls.append([photo_source_url, photo_url])
-    return photo_urls
+        url_pairs.append([photo_source_url, photo_url])
+    return url_pairs
 
 
 laguna_beach_photo_urls = get_photos_by_location(33.5422, -117.7831)

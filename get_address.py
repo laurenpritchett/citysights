eb877ed06e75fe988a98c9214eae6d1bba125b0e
Lauren Pritchett <laurenpritchett2@gmail.com>
2017-02-15 01:41:44 +0000

    Added doctest to get_address.py

--- get_address.py
+++ get_address.py
@@ -7,12 +7,17 @@ gmaps = googlemaps.Client(key=google_maps_api_key)
 
 
 def get_address_by_lat_lng(lat, lng):
-    """Turn lat/lng into human-readable address."""
+    """Turn lat/lng into human-readable address.
+
+    >>> get_address_by_lat_lng(37.808480, -122.475853)
+    u'Coastal Trail, San Francisco, CA 94129, USA'
+
+    """
 
     reverse_geocode_result = gmaps.reverse_geocode((lat, lng))
 
     # Return formatted address
-    # Looks like 'Coastal Trail, San Francisco, CA 94129, USA'
+    # Looks like u'Coastal Trail, San Francisco, CA 94129, USA'
 
     return reverse_geocode_result[0]['formatted_address']
 
